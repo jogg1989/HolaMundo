e561cff3d4c1d0bc3b2d65876a5e8b342500e155
jogg1989 <ing.jogg1989@gmail.com>
2021-11-05 22:52:27 -0600

    Completo

--- HolaMundo/FrmMain.Designer.cs
+++ HolaMundo/FrmMain.Designer.cs
@@ -29,20 +29,129 @@ namespace HolaMundo
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.lblText1 = new System.Windows.Forms.Label();
+            this.lblText2 = new System.Windows.Forms.Label();
+            this.tbPw1 = new System.Windows.Forms.TextBox();
+            this.tbPw2 = new System.Windows.Forms.TextBox();
+            this.btnValidate = new System.Windows.Forms.Button();
+            this.cbView1 = new System.Windows.Forms.CheckBox();
+            this.cbView2 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
+            // 
+            // lblText1
+            // 
+            this.lblText1.AutoSize = true;
+            this.lblText1.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.lblText1.Location = new System.Drawing.Point(108, 35);
+            this.lblText1.Name = "lblText1";
+            this.lblText1.Size = new System.Drawing.Size(132, 38);
+            this.lblText1.TabIndex = 0;
+            this.lblText1.Text = "Password";
+            // 
+            // lblText2
+            // 
+            this.lblText2.AutoSize = true;
+            this.lblText2.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.lblText2.Location = new System.Drawing.Point(12, 108);
+            this.lblText2.Name = "lblText2";
+            this.lblText2.Size = new System.Drawing.Size(228, 38);
+            this.lblText2.TabIndex = 1;
+            this.lblText2.Text = "Repetir Password";
+            // 
+            // tbPw1
+            // 
+            this.tbPw1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbPw1.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.tbPw1.Location = new System.Drawing.Point(246, 32);
+            this.tbPw1.Name = "tbPw1";
+            this.tbPw1.Size = new System.Drawing.Size(813, 45);
+            this.tbPw1.TabIndex = 2;
+            // 
+            // tbPw2
+            // 
+            this.tbPw2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbPw2.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.tbPw2.Location = new System.Drawing.Point(246, 101);
+            this.tbPw2.Name = "tbPw2";
+            this.tbPw2.Size = new System.Drawing.Size(813, 45);
+            this.tbPw2.TabIndex = 3;
+            // 
+            // btnValidate
+            // 
+            this.btnValidate.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnValidate.AutoSize = true;
+            this.btnValidate.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.btnValidate.Location = new System.Drawing.Point(527, 188);
+            this.btnValidate.Name = "btnValidate";
+            this.btnValidate.Size = new System.Drawing.Size(187, 65);
+            this.btnValidate.TabIndex = 4;
+            this.btnValidate.Text = "Validar";
+            this.btnValidate.UseVisualStyleBackColor = true;
+            this.btnValidate.Click += new System.EventHandler(this.btnValidate_Click);
+            // 
+            // cbView1
+            // 
+            this.cbView1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbView1.AutoSize = true;
+            this.cbView1.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.cbView1.Location = new System.Drawing.Point(1100, 35);
+            this.cbView1.Name = "cbView1";
+            this.cbView1.Size = new System.Drawing.Size(83, 42);
+            this.cbView1.TabIndex = 5;
+            this.cbView1.Text = "Ver";
+            this.cbView1.UseVisualStyleBackColor = true;
+            this.cbView1.CheckedChanged += new System.EventHandler(this.cbView1_CheckedChanged);
+            // 
+            // cbView2
+            // 
+            this.cbView2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbView2.AutoSize = true;
+            this.cbView2.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.cbView2.Location = new System.Drawing.Point(1100, 104);
+            this.cbView2.Name = "cbView2";
+            this.cbView2.Size = new System.Drawing.Size(83, 42);
+            this.cbView2.TabIndex = 6;
+            this.cbView2.Text = "Ver";
+            this.cbView2.UseVisualStyleBackColor = true;
+            this.cbView2.CheckedChanged += new System.EventHandler(this.cbView2_CheckedChanged);
             // 
             // FrmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(1231, 265);
+            this.Controls.Add(this.cbView2);
+            this.Controls.Add(this.cbView1);
+            this.Controls.Add(this.btnValidate);
+            this.Controls.Add(this.tbPw2);
+            this.Controls.Add(this.tbPw1);
+            this.Controls.Add(this.lblText2);
+            this.Controls.Add(this.lblText1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "¡Hola Mundo!";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblText1;
+        private System.Windows.Forms.Label lblText2;
+        private System.Windows.Forms.TextBox tbPw1;
+        private System.Windows.Forms.TextBox tbPw2;
+        private System.Windows.Forms.Button btnValidate;
+        private System.Windows.Forms.CheckBox cbView1;
+        private System.Windows.Forms.CheckBox cbView2;
     }
 }
 
--- HolaMundo/FrmMain.cs
+++ HolaMundo/FrmMain.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,51 @@ namespace HolaMundo
         public FrmMain()
         {
             InitializeComponent();
+            tbPw1.UseSystemPasswordChar = !cbView1.Checked;
+            tbPw2.UseSystemPasswordChar = !cbView2.Checked;
+        }
+
+        private void cbView1_CheckedChanged(object sender, EventArgs e)
+        {
+            tbPw1.UseSystemPasswordChar = !cbView1.Checked;
+            cbView2.Checked = cbView1.Checked;
+        }
+
+        private void cbView2_CheckedChanged(object sender, EventArgs e)
+        {
+            tbPw2.UseSystemPasswordChar = !cbView2.Checked;
+            cbView1.Checked = cbView2.Checked;
+        }
+
+        private void btnValidate_Click(object sender, EventArgs e)
+        {
+            if(Validar(tbPw1.Text, tbPw2.Text, out string mensaje))
+            {
+                MessageBox.Show(this, "Contraseña Válida", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show(this, mensaje, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool Validar(string cadena1, string cadena2, out string mensaje)
+        {
+            mensaje = "";
+            Regex regMay, regMin, regNum, regEsp;
+            regMay = new Regex("^(?=.*[A-Z]).{1,}$");
+            regMin = new Regex("^(?=.*[a-z]).{1,}$");
+            regNum = new Regex("^(?=.*\\d).{1,}$");
+            regEsp = new Regex("^(?=.*\\W).{1,}$");
+
+            mensaje += regMay.IsMatch(cadena1) ? "" : "Debe haber por lo menos una Mayúscula\n";
+            mensaje += regMin.Match(cadena1).Success ? "" : "Debe haber por lo menos una Minúscula\n";
+            mensaje += regNum.IsMatch(cadena1) ? "" : "Debe haber por lo menos una Número\n";
+            mensaje += regEsp.IsMatch(cadena1) ? "" : "Debe haber por lo menos una Caracter Especial\n";
+
+            mensaje += cadena1 == cadena2 ? "" : "Debes repetir el password\n";
+
+            return mensaje == "";
         }
     }
 }
